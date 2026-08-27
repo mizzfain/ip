968db302c78e3c6a0bf10959d3027c1640ac965b
Kevin Tan <kevintanshinwei@gmail.com>
2026-08-28 01:16:07 +0800

    Add echo feature

--- src/main/java/Kevin.java
+++ src/main/java/Kevin.java
@@ -1,9 +1,21 @@
+import java.util.Scanner;
+
 public class Kevin {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         String banner = "Hello! I'm Kevin.\n"
-                + "What would you like me to help you with?\n"
-                + "\n"
-                + "Bye. Hope I was of assistance to you!\n";
+                + "What would you like me to help you with?\n";
+
         System.out.println(banner);
+
+        String input = scanner.nextLine();
+
+        while (!input.equals("bye")) {
+            System.out.println(input + "\n");
+            input = scanner.nextLine();
+        }
+        String ending = "Bye. Hope I was of assistance to you!";
+        System.out.println(ending);
     }
 }
