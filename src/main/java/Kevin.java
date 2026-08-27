d1a91027ddb5ead616c966906f0e50a0b827a998
Kevin Tan <kevintanshinwei@gmail.com>
2026-08-28 00:53:49 +0800

    Add greeting

--- src/main/java/Kevin.java
+++ src/main/java/Kevin.java
@@ -1,10 +1,9 @@
 public class Kevin {
     public static void main(String[] args) {
-        String banner = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+        String banner = "Hello! I'm Kevin.\n"
+                + "What would you like me to help you with?\n"
+                + "\n"
+                + "Bye. Hope I was of assistance to you!\n";
         System.out.println(banner);
     }
 }
