c55ffc7ed174b8c0a659a78f47e88bcd7aeae0ab
Kevin Tan <kevintanshinwei@gmail.com>
2026-08-31 17:30:29 +0800

    Add ToDo class

---
+++ src/main/java/ToDo.java
@@ -0,0 +1,15 @@
+public class ToDo extends Task {
+
+    public ToDo(String description) {
+        super(description);
+    }
+
+    @Override
+    public String toString() {
+        if (isDone) {
+            return "[T][X] " + this.description;
+        } else {
+            return "[T][ ] " + this.description;
+        }
+    }
+}
