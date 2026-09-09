51e1e4e4991a9814d177dc7b7dd039c3d638e8a3
Kevin Tan <kevintanshinwei@gmail.com>
2026-09-09 22:35:19 +0800

    Return Exception message as string in respond()

--- src/main/java/kevin/Kevin.java
+++ src/main/java/kevin/Kevin.java
@@ -41,89 +41,88 @@ public class Kevin {
         Parser parser = new Parser(ui.start());
 
         while (parser.isNotBye()) {
-            try {
-                String response = respond(parser);
-                ui.print(response);
-                parser = new Parser(ui.readNextLine());
-            } catch (KevinException e) {
-                ui.print(e.getMessage() + '\n');
-                parser = new Parser(ui.readNextLine());
-            }
+            String response = respond(parser);
+            ui.print(response);
+            parser = new Parser(ui.readNextLine());
         }
         ui.end();
     }
 
-    public String respond(Parser parser) throws KevinException {
-        if (parser.isList()) {
-            return tasks.list();
+    public String respond(Parser parser) {
+        try {
+            if (parser.isList()) {
+                return tasks.list();
 
-        } else if (parser.startsWith("mark")) {
-            int taskIndex = parser.parseIndex("mark");
-            Task markedTask = tasks.mark(taskIndex);
-            storage.save(tasks);
+            } else if (parser.startsWith("mark")) {
+                int taskIndex = parser.parseIndex("mark");
+                Task markedTask = tasks.mark(taskIndex);
+                storage.save(tasks);
 
-            return "Nice! I've marked this task as done:\n  "
-                    + markedTask + "\n";
+                return "Nice! I've marked this task as done:\n  "
+                        + markedTask + "\n";
 
-        } else if (parser.startsWith("unmark")) {
-            int taskIndex = parser.parseIndex("unmark");
-            Task unmarkedTask = tasks.unmark(taskIndex);
-            storage.save(tasks);
+            } else if (parser.startsWith("unmark")) {
+                int taskIndex = parser.parseIndex("unmark");
+                Task unmarkedTask = tasks.unmark(taskIndex);
+                storage.save(tasks);
 
-            return "OK, I've marked this task as not done yet:\n  "
-                    + unmarkedTask + "\n";
+                return "OK, I've marked this task as not done yet:\n  "
+                        + unmarkedTask + "\n";
 
-        } else if (parser.startsWith("todo")) {
-            String description = parser.parseToDo();
-            ToDo todo = new ToDo(description);
-            tasks.add(todo);
-            storage.save(tasks);
+            } else if (parser.startsWith("todo")) {
+                String description = parser.parseToDo();
+                ToDo todo = new ToDo(description);
+                tasks.add(todo);
+                storage.save(tasks);
 
-            return "Got it. I've added this task:\n  " + todo
-                    + "\nNow you have " + tasks.size() + " tasks in the list.\n";
+                return "Got it. I've added this task:\n  " + todo
+                        + "\nNow you have " + tasks.size() + " tasks in the list.\n";
 
-        } else if (parser.startsWith("deadline")) {
-            Matcher matcher = parser.parseDeadline();
+            } else if (parser.startsWith("deadline")) {
+                Matcher matcher = parser.parseDeadline();
 
-            String description = matcher.group("description");
-            LocalDateTime byDateTime = parseDateTimeString(matcher.group("by"));
+                String description = matcher.group("description");
+                LocalDateTime byDateTime = parseDateTimeString(matcher.group("by"));
 
-            Deadline deadline = new Deadline(description, byDateTime);
-            tasks.add(deadline);
-            storage.save(tasks);
+                Deadline deadline = new Deadline(description, byDateTime);
+                tasks.add(deadline);
+                storage.save(tasks);
 
-            return "Got it. I've added this task:\n  " + deadline
-                    + "\nNow you have " + tasks.size() + " tasks in the list.\n";
+                return "Got it. I've added this task:\n  " + deadline
+                        + "\nNow you have " + tasks.size() + " tasks in the list.\n";
 
-        } else if (parser.startsWith("event")) {
-            Matcher matcher = parser.parseEvent();
+            } else if (parser.startsWith("event")) {
+                Matcher matcher = parser.parseEvent();
 
-            String description = matcher.group("description");
-            LocalDateTime from = parseDateTimeString(matcher.group("from"));
-            LocalDateTime to = parseDateTimeString(matcher.group("to"));
+                String description = matcher.group("description");
+                LocalDateTime from = parseDateTimeString(matcher.group("from"));
+                LocalDateTime to = parseDateTimeString(matcher.group("to"));
 
-            Event event = new Event(description, from, to);
-            tasks.add(event);
-            storage.save(tasks);
+                Event event = new Event(description, from, to);
+                tasks.add(event);
+                storage.save(tasks);
 
-            return "Got it. I've added this task:\n  " + event
-                    + "\nNow you have " + tasks.size() + " tasks in the list.\n";
+                return "Got it. I've added this task:\n  " + event
+                        + "\nNow you have " + tasks.size() + " tasks in the list.\n";
 
-        } else if (parser.startsWith("delete")) {
-            int taskIndex = parser.parseIndex("delete");
-            Task deletedTask = tasks.delete(taskIndex);
-            storage.save(tasks);
+            } else if (parser.startsWith("delete")) {
+                int taskIndex = parser.parseIndex("delete");
+                Task deletedTask = tasks.delete(taskIndex);
+                storage.save(tasks);
 
-            return "Noted. I've removed this task:\n  "
-                    + deletedTask + "\nNow you have " + tasks.size()
-                    + " tasks in the list.\n";
+                return "Noted. I've removed this task:\n  "
+                        + deletedTask + "\nNow you have " + tasks.size()
+                        + " tasks in the list.\n";
 
-        } else if (parser.startsWith("find")) {
-            String keyword = parser.parseKeyword();
-            return tasks.find(keyword);
+            } else if (parser.startsWith("find")) {
+                String keyword = parser.parseKeyword();
+                return tasks.find(keyword);
 
-        } else {
-            throw new KevinException("??? Sorry but I don't speak gibberish.");
+            } else {
+                return "??? Sorry but I don't speak gibberish.\n";
+            }
+        } catch (KevinException e) {
+            return e.getMessage() + '\n';
         }
     }
 
