19e5de6e433e3b68e28636c5757319f6e4765fb8
Kevin Tan <kevintanshinwei@gmail.com>
2026-09-10 19:39:26 +0800

    Add assert in save() in Storage.java
    Ensures parent (data) exists and is a folder. Should have been created
    by load() before any attempts to save().

--- src/main/java/kevin/Storage.java
+++ src/main/java/kevin/Storage.java
@@ -21,14 +21,11 @@ public class Storage {
         this.filePath = Paths.get(filePathString);
     }
 
+
     /**
-     * Loads TaskList from tasks.txt.
+     * Loads TaskList from filePath.
      * @return TaskList
-     */
-    /**
-     * Loads TaskList from tasks.txt.
-     * @return TaskList
-     * @throws KevinException If tasks.txt does not exist.
+     * @throws KevinException If filePath does not exist.
      */
     public TaskList load() throws KevinException {
         TaskList tasks = new TaskList();
@@ -52,20 +49,15 @@ public class Storage {
     }
 
     /**
-     * Saves tasks into tasks.txt.
+     * Saves tasks into filePath.
      * @param TaskList tasks
      */
     public void save(TaskList tasks) {
-        try {
-            Path folderPath = filePath.getParent();
+        Path folderPath = filePath.getParent();
 
-            if (folderPath != null) {
-                Files.createDirectories(folderPath);
-            }
+        //Checks that parent (data) exists and is a folder
+        assert Files.exists(folderPath) && Files.isDirectory(folderPath);
 
-            tasks.save(filePath);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        tasks.save(filePath);
     }
 }
