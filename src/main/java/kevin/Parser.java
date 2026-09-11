c5c87a43e56982e6a079c3cb1fc699b36b21d914
Kevin Tan <kevintanshinwei@gmail.com>
2026-09-11 15:06:01 +0800

    Abstract out parseInput

--- src/main/java/kevin/Parser.java
+++ src/main/java/kevin/Parser.java
@@ -43,9 +43,7 @@ public class Parser {
      * @throws KevinException If no index in input.
      */
     public int parseIndex(String command) throws KevinException {
-        Pattern pattern = Pattern.compile(command + "\\s+(\\d+)");
-        Matcher matcher = pattern.matcher(input);
-
+        Matcher matcher = parseInput(command + "\\s+(\\d+)");
         if (matcher.matches()) {
             return Integer.parseInt(matcher.group(1)) - 1;
         } else {
@@ -54,9 +52,7 @@ public class Parser {
     }
 
     public String parseKeyword() throws KevinException {
-        Pattern pattern = Pattern.compile("^find\\s+(.+)");
-        Matcher matcher = pattern.matcher(input);
-
+        Matcher matcher = parseInput("^find\\s+(.+)");
         if (matcher.matches()) {
             return matcher.group(1);
         } else {
@@ -71,9 +67,7 @@ public class Parser {
      * @throws KevinException If input does not have a description.
      */
     public String parseToDo() throws KevinException {
-        Pattern pattern = Pattern.compile("^todo\\s+(?<description>.+?)$");
-        Matcher matcher = pattern.matcher(input);
-
+        Matcher matcher = parseInput("^todo\\s+(?<description>.+?)$");
         if (matcher.matches()) {
             return matcher.group("description");
         } else {
@@ -88,8 +82,7 @@ public class Parser {
      */
     public Matcher parseDeadline() throws KevinException {
         String regex = "^deadline\\s+(?<description>.+?)\\s+/by\\s+(?<by>.+)$";
-        Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(input);
+        Matcher matcher = parseInput(regex);
 
         if (matcher.matches()) {
             return matcher;
@@ -107,13 +100,17 @@ public class Parser {
         String regex = "^event\\s+(?<description>.+?)\\s+"
                 + "/from\\s+(?<from>.+?)\\s+"
                 + "/to\\s+(?<to>.+)$";
-        Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(input);
+        Matcher matcher = parseInput(regex);
 
         if (matcher.matches()) {
             return matcher;
         } else {
             throw new KevinException("Event does not have a description, from or to date.");
         }
+    }
+
+    public Matcher parseInput(String regex) {
+        Pattern pattern = Pattern.compile(regex);
+        return pattern.matcher(input);
     }
 }
